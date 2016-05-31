097b1bd4cd8f65eb3388db2f6de5e3a99b07f469
zangrafx <zangrafx@gmail.com>
2016-05-31 20:37:37 +0200

    move state to baobab

--- src/Wrapper.jsx
+++ src/Wrapper.jsx
@@ -10,61 +10,22 @@ import ResultsPanel from "src/ResultsPanel";
 import SearchPanel from "src/SearchPanel";
 
 export default React.createClass({
-    "getInitialState": function () {
-        return {
-            "filter": "",
-            "sortBy": ""
-        };
-    },
     "propTypes": {
         "Books": React.PropTypes.array.isRequired
     },
-    "setFilterValue": function (value) {
-        this.setState({
-            "filter": value
-        });
-    },
-    "setSortByValue": function (value) {
-        this.setState({
-            "sortBy": value
-        });
-    },
-    "containsFilterValue": function (word, value) {
-        return word.indexOf(value) !== -1;
-    },
-    "filterList": function (list) {
-        return list.filter(
-            element => this.containsFilterValue(element.Title.toLowerCase(), this.state.filter)
-        );
-    },
-    "sortList": function (list) {
-        return list.sort(
-            (a, b) => {
-                var A = a[this.state.sortBy].toLowerCase();
-                var B = b[this.state.sortBy].toLowerCase();
-                if (A < B) {
-                    return -1;
-                }
-                if (A > B) {
-                    return 1;
-                }
-                return 0;
-            }
-        );
-    },
     "render": function () {
-        var Results = this.filterList(this.props.Books);
-        if (this.state.sortBy && Results) {
-            Results = this.sortList(Results);
+        var Results = this.props.filterList(this.props.Books);
+        if (this.props.sortBy && Results) {
+            Results = this.props.sortList(Results);
         }
         return (
             <div>
                 <h1>List of books</h1>
-                <SearchPanel setFilterValue={this.setFilterValue} />
+                <SearchPanel setFilterValue={this.props.setFilterValue} />
                 <ResultsPanel
                     Results={Results}
-                    setSortByValue={this.setSortByValue}
-                    sortBy={this.state.sortBy}
+                    setSortByValue={this.props.setSortByValue}
+                    sortBy={this.props.sortBy}
                 />
             </div>
         );
